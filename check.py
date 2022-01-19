bb4acaeb2198ca8e265ce9328620ed54fb8d8e81
Alex <alexprengere@gmail.com>
2022-01-19 09:02:23 +0100

    Update checker script

--- check.py
+++ check.py
@@ -1,10 +1,12 @@
 import sys
 from neobase import NeoBase
 
+NeoBase.skip = lambda *_: False
+
 with open("optd_por_public.csv") as f:
     N = NeoBase(f)
 
 for row in sys.stdin:
-    name, country, key = row.rstrip().split(",")
+    city_name, name, key = row.rstrip().split(",")
     if key not in N:
-        print(key, name)
+        print(f"- [ ] `{key}`: {name} ({city_name})")
